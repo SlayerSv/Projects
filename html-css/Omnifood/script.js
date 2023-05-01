05cf54fdced4094758cd84dfccdee3230492a11e
SlayerSv <slayer-sv@mail.ru>
2023-05-02 05:29:44 +0700

    added navigation favicon

--- html-css/Omnifood/script.js
+++ html-css/Omnifood/script.js
@@ -1,6 +1,12 @@
 menuButton = document.querySelector(".menu-btn");
 closeButton = document.querySelector(".close-btn");
 navBar = document.querySelector(".nav-bar");
+links = document.querySelectorAll(".nav-link");
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    navBar.classList.remove("open");
+  })
+});
 
 menuButton.addEventListener("click", () => {
   navBar.classList.add("open");
