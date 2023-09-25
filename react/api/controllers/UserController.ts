91fb44add129f1a81de2cdaf5e6ef91a57bdd999
SlayerSv <slayer-sv@mail.ru>
2023-09-25 22:55:34 +0700

    added signup

--- react/api/controllers/UserController.ts
+++ react/api/controllers/UserController.ts
@@ -1,22 +1,21 @@
-import sequelize from "../db";
+import sequelize from "../models/associations";
 import ApiError from "./ErrorController";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { Op } from "sequelize";
 
-const User = sequelize.models.userModel;
+const User = sequelize.models.User;
 
 class UserController {
 
-  async create(req: any, res: any, next: any) {
-    try {
-      const user = await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        avatar: req.body.avatar
-      });
-      res.status(201).send(user);
-    } catch (e) {
-      return next(e);
-    }
+  async create(name: string, email: string, password: string, avatar: string) {
+    const user = await User.create({
+      name,
+      email,
+      password,
+      avatar
+    });
+    return user;
   }
 
   async getUser(req: any, res: any, next: any) {
@@ -30,4 +29,37 @@ class UserController {
       return next(e);
     }
   }
+
+  async signup(req: any, res: any, next: any) {
+    const {name, email, password, avatar} = req.body;
+    try {
+      if (!name || !email || !password) {
+        return next(ApiError.notFound("required values are not provided"))
+      }
+      const candidate = await User.findOne({
+        where: {
+          [Op.or]: [
+            {email},
+            {name}
+          ]
+        }
+      })
+      if (candidate) {
+        return next(ApiError.notFound("Such user already exists"));
+      }
+      const hashedPassword = bcrypt.hashSync(req.body.password, 5);
+      const user:any = await this.create(name, email, hashedPassword, avatar)
+      const accessToken = jwt.sign(
+        {id: user.id, email: user.email, name: user.name},
+        process.env.JWT_SECRET_KEY as string,
+        {expiresIn: "24h"}
+      )
+      return res.status(201).send(accessToken);
+    } catch (e) {
+      console.log(e);
+      next(e)
+    }
+  }
 }
+
+export default new UserController();
--- react/api/routes/usersRouter.ts
+++ react/api/routes/usersRouter.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import UserController from "../controllers/UserController";
 
 const router = Router();
 
@@ -8,5 +9,7 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   res.send(`USER ${req.params.id}`)
 });
+router.post("/signup", UserController.signup.bind(UserController));
+router.post("/signin", )
 
 export default router;
